6a47cf41c2fddb2c6c14271a56a14d4a5167b359
Nelson Londono <nelsonlondonodev@gmail.com>
2024-10-19 08:36:36 +0200

    cambie setTimeOut en el h1

---
+++ functions.js
@@ -0,0 +1,181 @@
+// https://github.com/makeitrealcamp/ejercicios-javascript
+
+// Ejercicios de javaScript en Github
+
+/*1. Contraseña válida ✅
+Escribir una función llamada contrasenaValida que reciba un string y retorne true si el string es igual a "2Fj(jjbFsuj" o "eoZiugBf&g9". De lo contrario debe retornar false.*/
+
+// const validarClave = document
+//   .getElementById("btn11")
+//   .addEventListener("click", validarKeys);
+
+// function validarKeys() {
+//   const contraseñaUno = "2Fj(jjbFsuj";
+//   const contraseñaDos = "eoZiugBf&g9";
+
+//   // let mensajePromt = prompt("Introduce tu contraseña aquí");
+
+//   if (mensajePromt === "" || mensajePromt === null) {
+//     alert("No has introducido ninguna contraseña");
+//   } else if (mensajePromt === contraseñaUno || mensajePromt === contraseñaDos) {
+//     alert("True");
+//   } else {
+//     alert("False");
+//   }
+
+//   return;
+// }
+
+// validarKeys();
+
+/**
+ * 2. Calcular impuestos ✅
+Escribir una función llamada calcularImpuestos que reciba dos argumentos numéricos: edad e ingresos. Si edad es igual o mayor a 18 y los ingresos son iguales o mayores a 1000 debe retornar ingresos * 40%. De lo contrario retornar 0.
+ */
+
+// const calcular = document
+//   .getElementById("btn12")
+//   .addEventListener("click", calcularImpuestos);
+
+// function calcularImpuestos() {
+//   //  let edad = Number(prompt("Introduce tu edad"));
+//   //  let ingresos = Number(prompt("Introduce tus ingresos"));
+
+//   if (edad === "" || ingresos === "" || isNaN(edad) || isNaN(ingresos)) {
+//     alert("No has introducido ninguna edad o ingresos");
+//   } else if (edad >= 18 && ingresos >= 1000) {
+//     alert(ingresos * 0.4);
+//   } else {
+//     alert(0);
+//   }
+// }
+
+// calcularImpuestos();
+
+/**
+ * 3. IMC (ïndice de masa corporal) ✅
+El índice de masa corporal (IMC), o BMI por sus siglas en inglés, es un valor que determina la cantidad de grasa de una persona.
+
+El BMI se calcula con la siguiente formula: peso / altura^2
+
+Escribir una función llamada bmi que reciba dos argumentos: peso y altura, y retorne un string con las siguientes posibilidades:
+
+"Bajo de peso" si el BMI < 18.5
+"Normal" si está entre 18.5 y 24.9
+"Sobrepeso" si está entre 25 y 29.9
+"Obeso" si es igual o mayor a 30
+ */
+
+// const eventoMasa = document
+//   .getElementById("btn13")
+//   .addEventListener("click", calcularMasaCorporal);
+
+// function calcularMasaCorporal() {
+//   // let peso = Number(prompt("¿Cual es tú peso?"));
+//   // let altura = Number(prompt("¿Cual es tu altura?"));
+//   let bmi = peso / (altura * 2);
+
+//   if (peso === "" || altura === "") {
+//     alert("No has introducido valor válido");
+//     return;
+//   } else if (bmi < 18.5) {
+//     alert("Bajo de peso ❌");
+//     return;
+//   } else if (bmi >= 18.5 && bmi < 24.9) {
+//     alert("Normal 💪🏼");
+//     return;
+//   } else if (bmi >= 25 && bmi < 29.9) {
+//     alert("Sobrepeso 🏋🏽‍♀️");
+//     return;
+//   } else if (bmi >= 30) {
+//     alert("Obeso 🏋🏽‍♀️");
+//     return;
+//   }
+// }
+
+// calcularMasaCorporal();
+
+// 4. Imprimir un arreglo ❌
+// Escribir una función llamada imprimirArreglo que reciba un arreglo e imprima cada elemento en una línea a parte:
+
+// function imprimirArreglo() {
+//   let arregloNuevo = [1, "Hola", 2, "Mundo"];
+
+//   console.log(arregloNuevo);
+// }
+
+// imprimirArreglo();
+
+// 5. Número de Likes
+// Escribe una función llamada likes que reciba un número y retorne un string utilizando el formato de K para miles y M para millones.
+
+// Por ejemplo:
+
+// 1400 se convierte en 1K
+// 34,567 se convierte en 34K
+// 7’456,345 se convierte en 7M.
+// Si el número es menor a 1000 se debe devolver el mismo número como un string
+
+// function likes() {
+//   let numero = Number(prompt("¿Cual es el número de likes?"));
+//   let resultado = Math.floor(numero);
+
+//   if (resultado <= 999) {
+//     console.log(`Tienes ${resultado} likes, en tus redes sociales`);
+//   } else if (resultado >= 1000) {
+//     console.log(`Tienes ${resultado}K likes, en tus redes sociales`);
+//   }
+// }
+
+// likes();
+
+// 6. FizzBuzz ❌
+// Escribir una función llamada fizzBuzz que reciba un número y retorne un string de acuerdo a lo siguiente:
+
+// "fizz" si el número es múltiplo de 3.
+// "buzz" si el número es múltiplo de 5.
+// "fizzbuzz" si el número es múltiplo tanto de 3 como de 5.
+// Si no cumple ninguna de las condiciones anteriores debe retornar el mismo número.
+
+// 7. Contar rango de números
+// Escribir una función llamada contarRango que reciba dos números y retorne cuántos números que hay entre ellos (excluyéndolos):
+
+// Nota: Utiliza un ciclo en tu solución. Puedes asumir que el primer número va a ser menor que el segundo.
+
+// function contarRango() {
+//   let numero1 = Number(prompt("¿Escribe un numero?"));
+//   let numero2 = Number(prompt("¿Escribe un numero?"));
+//   for (let i = numero1; i < numero2; i++) {
+//     console.log(
+//       `'Entre el numero ${numero1} y el numero ${numero2} tenemos ${
+//         numero2 - numero1 - 1
+//       } numeros'`
+//     );
+//   }
+// }
+
+// contarRango();
+
+// 8. Sumar rango de números
+// Escribir una función llamada sumarRango que reciba dos argumentos: número inicial y número final. La función debe retornar la suma de los números en ese rango (incluyéndolos).
+
+// Nota: puedes asumir que el número inicial va a ser menor o igual que el número final.
+
+function sumarRango() {
+  let minimo = Number(prompt("¿Escribe un numero?"));
+  let maximo = Number(prompt("¿Escribe un numero?"));
+  let suma = 0;
+
+  console.log(minimo, maximo);
+
+  if (minimo === true || maximo === true) {
+    window: alert("No haz ingresado un numero aun.");
+  } else {
+    for (let i = minimo; i <= maximo; i++) {
+      suma += i;
+    }
+    console.log(suma);
+  }
+}
+
+sumarRango();
--- main.js
+++ main.js
@@ -1,66 +1,305 @@
 //================== LLamado por ID ==============//
 
-const llamado = document.getElementById("btn1");
-const crearParrafo = document.getElementById("parrafo-saludo");
-const cambioBg = document.getElementById("btn2");
-const cambioBgBody = document.getElementById("body");
-const cambioBgBt = document.getElementById("btn2");
-const cambioColorText = document.getElementById("parrafo-saludo");
-const eliminar = document.getElementById("divP");
+// const llamado = document.getElementById("btn1");
+// const crearParrafo = document.getElementById("parrafo-saludo");
+// const cambioBg = document.getElementById("btn2");
+// const cambioBgBody = document.getElementById("body");
+// const cambioBgBt = document.getElementById("btn2");
+// const cambioColorText = document.getElementById("parrafo-saludo");
+// const eliminar = document.getElementById("otro-parrafo");
+// const crearNuevoElemento = document.getElementById("btn4");
+// const enviarEmailForm = document.getElementById("exampleInputEmail1");
+// const btnEnviar = document.getElementById("btnForm");
+// const btnEdad = document.getElementById("btn6");
+// const parrafoEdad = document.getElementById("divP2");
+// const btnParImpar = document.getElementById("btn7");
 
-//================= Eventos =======================//
+// //================= Eventos =======================//
 
-llamado.document.addEventListener("click", clickBtn);
-cambioBg.document.addEventListener("click", clickBtn2);
-eliminar.document.addEventListener("click", limpiarHtml);
+// const eventoPromt = llamado.document.addEventListener("click", clickBtn);
+// const eventoCbBg = cambioBg.document.addEventListener("click", clickBtn2);
+// const eventoEliminar = eliminar.document.addEventListener("click", limpiarHtml);
+// const eventoCrear = crearNuevoElemento.document.addEventListener(
+//   "click",
+//   crearNuevosElementos
+// );
+// const eventoEmail = btnEnviar.document.addEventListener("click", enviarEmail);
+// const eventoEdad = btnEdad.document.addEventListener("click", pedirEdad);
+// const eventoParrafoEdad = parrafoEdad.document.addEventListener("click");
+// const eventoParImpar = btnParImpar.document.addEventListener(
+//   "click",
+//   numerosParImpar
+// );
+// const eventoNuevoParrafo = document.addEventListener("click", nuevotexto);
 
-//================= Variables ====================//
+// //================= Crear nodos ===============================//
 
-//================= Crear elementos ==============//
-
-function clickBtn() {
-  let saludoPrompt = prompt("Hola, ¿Cúal es tú nombre?");
-  if (saludoPrompt === "") {
-    alert("No has introducido ningún nombre");
-  } else {
-    //============= Crear parrafo =============//
-    crearParrafo.innerText = saludoPrompt;
-    //alert(`"Hola ${saludoPrompt}"`);
-  }
+// //================= Funcion para cambiar h1 =================================//
+document.addEventListener("click", () => {
+  const mensajeCambioH1 = "Hola a todos, soy un h1";
+  const eventofinalH1 = (document.querySelector("#h1").innerText =
+    mensajeCambioH1);
+  //(cambiarH1.innerText = mensajeCambioH1);
 
   setTimeout(() => {
-    crearParrafo.innerText = `"Hola ${saludoPrompt}, preciona nuevamente el botón"`;
+    let removeChanges = document.lastModified;
+    document.querySelector("h1").innerText = removeChanges;
+    //document.removeEventListener("click", clickBtnH1());
+    //remove("mensajeCambioH1");
+    //cambiarH1.remove();
   }, 3000);
-  return;
-}
 
-clickBtn();
+  //document.getElementById("h1").reset("[]");
+});
 
-//================== Funcion para cambiar el background ===========//
+// //================= Función para crear elementos ==============//
 
-function clickBtn2() {
-  //================ Metodo para darle clases a los elementos=============//
-  cambioBgBody.classList.add("bg-secondary");
-  cambioBgBt.classList.add("bg-warning");
-  cambioColorText.classList.add("text-white");
+// function clickBtn() {
+//   let saludoPrompt = prompt("Hola, ¿Cúal es tú nombre?");
 
-  //====== Metodo para eliminar el las clases requeridas con Javascript de Bootstrap =======//
+//   if (saludoPrompt === "" || saludoPrompt === null) {
+//     alert("No has introducido ningún nombre");
+
+//     return;
+//   } else {
+//     //============= Crear parrafo =============//
+
+//     crearParrafo.innerText = saludoPrompt;
+//   }
+
+//   setTimeout(() => {
+//     crearParrafo.innerText = `"Hola ${saludoPrompt}, preciona nuevamente el botón"`;
+//   }, 3000);
+//   return;
+// }
+
+// clickBtn();
+
+// //================== Funcion para cambiar el background ===========//
+
+// function clickBtn2() {
+//   //================ Metodo para darle clases a los elementos=============//
+//   cambioBgBody.classList.add("bg-secondary");
+//   cambioBgBt.classList.add("bg-warning");
+//   cambioColorText.classList.add("text-white");
+
+//   //================ Metodo para eliminar las clases requeridas con Javascript de Bootstrap =======//
+//   setTimeout(() => {
+//     cambioBgBody.classList.remove("bg-secondary");
+//     cambioBgBt.classList.remove("bg-warning");
+//     cambioColorText.classList.remove("text-white");
+//   }, 3000);
+
+//   return;
+// }
+
+// clickBtn2();
+
+// //================== función para limpiar el html ============ //
+
+// function limpiarHtml() {
+//   alert("¿Quieres eliminar el parrafó?");
+//   eliminar.remove();
+
+//   return;
+// }
+
+// limpiarHtml();
+
+// //================== función para crear nuevos elementos =================//
+
+// function crearNuevosElementos() {
+//   //===================== Primer div =======================//
+
+//   const Fecha = new Date();
+//   const para = document.createElement("p");
+//   para.classList.add("text-success");
+//   const node = document.createTextNode(Fecha);
+//   para.appendChild(node);
+//   const elemento = document.getElementById("divPNuevo");
+//   elemento.appendChild(para);
+
+//   const imagenVerano = URL("image/summer image.webp");
+// }
+
+// crearNuevosElementos();
+
+// //==================== Función para email ======================//
+
+// function enviarEmail() {
+//   const emailCliente = document.getElementById("exampleInputEmail1").value;
+//   if (emailCliente === "") {
+//     alert("Por favor ingrese un email");
+//   } else {
+//     document.getElementById("parrafo-saludo").innerText = emailCliente;
+//     alert("Tu email fue enviado correctamente.");
+
+//     setTimeout(() => {
+//       document.getElementById("exampleInputEmail1").value = "";
+//     }, 100);
+//   }
+// }
+
+// enviarEmail();
+
+// //===================== Función edad ==================================//
+
+// function pedirEdad() {
+//   const nombre = prompt("¿Cúal es tu nombre?");
+//   const edad = prompt(`Hola ${nombre}, ¿Cuántos años tienes?`);
+//   const mensajeAñosNiño = `"${nombre} tienes ${edad} años. ¡Eres un niño! nececistas autorización de tus padres o acudiente."`;
+//   const mensajeAñosAdolecente = `"${nombre} tienes ${edad} años. ¡Eres un adolecente! aun no puedes obtener tu licencia de conducir."`;
+//   const mensajeAñosAdulto = `"${nombre} tienes ${edad} años. ¡Eres un adulto! ya puedes obtener tu licencia."`;
+//   const nombreStri = toString(nombre);
+//   const edadNumero = Number(edad);
+
+//   if (nombre === "" || nombre === null) {
+//     alert("Por favor ingrese un nombre");
+//     return;
+//   } else if (edad === "" || edad != edadNumero || edad === null) {
+//     document.getElementById("parrafo2").innerText =
+//       "Por favor ingrese un numero";
+
+//     return;
+//   } else if (edad < 12) {
+//     document.getElementById("parrafo2").innerText = mensajeAñosNiño;
+//   } else if ((edad >= 12) & (edad < 18)) {
+//     document.getElementById("parrafo2").innerText = mensajeAñosAdolecente;
+//   } else if (edad >= 18) {
+//     document.getElementById("parrafo2").innerText = mensajeAñosAdulto;
+//   }
+
+//   setTimeout(() => {
+//     document.getElementById("parrafo2").innerText = "";
+//   }, 3000);
+
+//   return;
+// }
+
+// pedirEdad();
+
+// //===================== Función para el botón de enviar numero ======================//
+
+// function numerosParImpar() {
+//   const numParImpar = document.getElementById("numeroParOImpar").value;
+//   console.log(numParImpar);
+//   if (numParImpar === "") {
+//     alert("Por favor ingrese un numero");
+//   } else if (numParImpar % 2 == 0) {
+//     document.getElementById(
+//       "parrafoParImpar"
+//     ).innerText = `"El numero ${numParImpar} es par"`;
+//   } else {
+//     numParImpar % 1 == 1;
+//     document.getElementById(
+//       "parrafoParImpar"
+//     ).innerText = `El numero ${numParImpar} es impar"`;
+//   }
+
+//   setTimeout(() => {
+//     document.getElementById("parrafoParImpar").remove();
+//   }, 3000);
+
+//   document.addEventListener("load");
+// }
+
+// numerosParImpar();
+
+// //===================== Función para cambiar carita ======================//
+
+// const eventoCarita = document.getElementById("imagenes");
+// eventoCarita.addEventListener("click", caritaFelizOn);
+// eventoCarita.addEventListener("click", caritaTristeOn);
+
+// function caritaFelizOn() {
+//   if ((click = true)) {
+//     const caritaFeliz = (document.getElementById("imagenes").src =
+//       "image/happy.png");
+//   }
+// }
+
+// cambiarCarita();
+
+// function caritaTristeOn() {
+//   if ((click = true)) {
+//     const caritaTriste = (document.getElementById("imagenes").src =
+//       "image/sad.png");
+//   }
+// }
+
+// caritaTristeOn();
+
+// /*========================== Calculadora ===================== */
+
+// /*========================== Dom calaculadora ==================*/
+
+// const pantalla = document.getElementById("div-calculadora");
+
+// /*========================== Evnetos calaculadora =============== */
+
+// const eventoInput = pantalla.addEventListener("click", funBtn7);
+
+// /*========================== Funciones =========================== */
+
+// function funBtn7() {
+//   let boton7 = document.getElementById("btn-7").value;
+//   if (boton7 === 7) {
+//     pantalla.innerHTML = boton7;
+//     console.log(boton7);
+//   }
+// }
+
+// funBtn7();
+
+// //============================= Nuevo div o texto ====================//
+
+// function nuevotexto() {
+//   const nuevoDiv = document.createElement("div");
+//   document.body.appendChild(nuevoDiv);
+
+//   const nuevoParrafo = document.createElement("p");
+//   nuevoParrafo.innerText = "Nuevo parrafó";
+//   document.body.appendChild(nuevoParrafo);
+// }
+
+//===================== Segundo div =======================//
+
+//const crearDIV = document.createElement("div");
+//const mostraDiv = (crearDIV.innerText = "Este es un nuevo div");
+//document.body.appendChild(mostraDiv);
+
+/**================== Formulario con bootstrap ===================*/
+
+// const submitBtn = document.querySelector(".form");
+// submitBtn.addEventListener("submit", (e) => {
+//   e.preventDefault();
+
+const inputEmail = document.querySelector("#InputEmail");
+inputEmail.addEventListener("input", (e) => {
+  console.log(e.target.value);
+});
+
+const inputPassword = document.querySelector("#InputPassword");
+inputPassword.addEventListener("input", functionPassword);
+
+function functionPassword() {
+  inputPassword.type = "text";
+
   setTimeout(() => {
-    cambioBgBody.classList.remove("bg-secondary");
-    cambioBgBt.classList.remove("bg-warning");
-    cambioColorText.classList.remove("text-white");
-  }, 3000);
-
-  return;
+    inputPassword.type = "password";
+  }, 300);
 }
 
-clickBtn2();
+const checkForm = document.querySelector(".form-check-label");
+checkForm.addEventListener("click", (e) => {
+  console.log("Check...");
+});
 
-//========== funcion para limpiar el html ============ //
+//   const inputPassword = document.querySelector("#exampleInputPassword2").value;
 
-function limpiarHtml() {
-  alert("¿Quieres eliminar todo?");
-  eliminar.remove();
-}
-
-limpiarHtml();
+//   if (inputEmail === "" || inputPassword === "") {
+//     console.log("Debes ingresar un correo valido");
+//   } else {
+//     console.log("Enviando datos...");
+//   }
+// });
